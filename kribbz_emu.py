879fd9f828098e8af52cf8237e1f464c600b0b42
sanko61 <assem1@yandex.ru>
2018-09-06 21:08:57 +0300

    transfer_coin2

--- kribbz_emu.py
+++ kribbz_emu.py
@@ -89,7 +89,7 @@ def transfer_coin():
         },
           "transfer": {
               "amount":55.121,
-              "destination_address":"ckbzzBGZpWBZiPyQmxN3NCK8DJ8S37Vhxb69a9pbYXgMAwKKPDGeZ93aZgSXfX1E3GMEbk6tgLGPK8gDAeGquLmASXKvRim7pzN",
+              "destination_address":"ckbzz7dYVt6T4sBWhjp8wqQUMgoE8hWyfFCcBjjdLrzEdJU9SQsXjm73rb4VzZt7RxGzuLCAs4ZtQQMJzeELHP841TbYDt4Rm9V",
               }
         },
     )
