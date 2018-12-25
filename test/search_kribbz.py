34d86b84d0bbff7cf47a97fe9ab1aa99f84fe0ad
sanko61 <assem1@yandex.ru>
2018-12-26 01:08:55 +0300

    add readme file

--- test/search_kribbz.py
+++ test/search_kribbz.py
@@ -9,7 +9,10 @@ import json
 from kribbz_emu import search_kribbz
 
 if __name__ == "__main__":
+    print ("wallet22:==========================================================================")
     r1 = search_kribbz(w_name="wallet22", pwd="Password12345", stateCode="CA", zipCode="90210" )
     print (r1)
+
+    print ("wallet23:==========================================================================")
     r1 = search_kribbz(w_name="wallet23", pwd="Password12345", stateCode="CA", zipCode="90210" )
     print (r1)
