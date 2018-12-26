d49a3a62332da49aa0f28921515575ce64c11657
sanko61 <assem1@yandex.ru>
2018-12-26 18:45:47 +0300

    add readme file

--- test/get_transfers.py
+++ test/get_transfers.py
@@ -9,5 +9,22 @@ import json
 from kribbz_emu import get_transfers
 
 if __name__ == "__main__":
-    r1 = get_transfers()
+    print ("==wallet22========================================")
+    r1 = get_transfers(w_name="wallet22", pwd="Password12345")
+    print (r1)
+
+    print ("==wallet23========================================")
+    r1 = get_transfers(w_name="wallet23", pwd="Password12345")
+    print (r1)
+
+    print ("==wallet24========================================")
+    r1 = get_transfers(w_name="wallet24", pwd="Password12345")
+    print (r1)
+
+    print ("==wallet7========================================")
+    r1 = get_transfers(w_name="wallet7", pwd="Password12345")
+    print (r1)
+
+    print ("==kribbz_wallet========================================")
+    r1 = get_transfers(w_name="kribbz_wallet", pwd="Password12345")
     print (r1)
