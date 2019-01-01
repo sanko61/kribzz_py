16618b7d93330b267028133249edc472825dd6b5
sanko61 <assem1@yandex.ru>
2019-01-01 16:31:17 +0300

    fix transfer_coin unittests

--- test/transfer_coin.py
+++ test/transfer_coin.py
@@ -21,10 +21,11 @@ if __name__ == "__main__":
     # "ckbzzAkpo6cY3hRGrVhyYFNaH9uowjUDhVwN2APDo8TRjWsBewnM39i5MXxJmnDAJkbYFVA6rxMAuTSACbZbAMwT5FzK3sQnLro",  # wallet7
     # "ckbzz7dYVt6T4sBWhjp8wqQUMgoE8hWyfFCcBjjdLrzEdJU9SQsXjm73rb4VzZt7RxGzuLCAs4ZtQQMJzeELHP841TbYDt4Rm9V"  wal6
     #  ckbzzBUC8KrZduCS9svqE7B1FvqBCpV2CBqkrLtqn6JmFHF9qQXyiE1Xf1aTd9YT6nbfqwZAhr5inGZVjZcdVVzSHPthTWMeNpV  kribbz_b
+    #  ckbzz7nUJw9KX3ky4JFa168Qu7PSoiwfqCDczt4BJo7CZHk7UmPWcDkhnfTUZZPvXpRhHDAHUtJe7hjuoZAeseXb6TnCC27gLad  kribbz2
 
     tx_id = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 #    r2 = transfer_coin(amnt=0.00000001, dst = "ckbzz6v3RFwV3s5je4PzcDGTZdsHy5Dg13dCrXwYDctYQi1UXYd3jVDPAfNHJmrSxx4TvMnh1pVUUNudRrqXEL7D46nY8D4Virp" , transactionId = tx_id)
-    r2 = transfer_coin(amnt=857477659.06, dst = "ckbzz7nUJw9KX3ky4JFa168Qu7PSoiwfqCDczt4BJo7CZHk7UmPWcDkhnfTUZZPvXpRhHDAHUtJe7hjuoZAeseXb6TnCC27gLad" , transactionId = tx_id)  #kribbz2
+    r2 = transfer_coin(amnt=0.00000001, dst = "ckbzz9ntUJj4mx3j75nAss3CzvCGXASnYTejDexmfC7k6f4XLqsMJUKN7i84SEz6CTR9LM4ggBMRUgXBHhAtUAgtCvWzv8dGjPr", wal_name = "kribbz2" , transactionId = tx_id)  #wallet24
 
     print (r2)
 
