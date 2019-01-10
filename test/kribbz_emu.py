0c85903aa0d4ae2312346f1daee800c273309469
sanko61 <assem1@yandex.ru>
2019-01-10 16:06:26 +0300

    fix4  transfer coin unittests with kribbz

--- test/kribbz_emu.py
+++ test/kribbz_emu.py
@@ -143,9 +143,43 @@ def transfer_full(transfer, kribbz= None, wal_name = "kribbz_wallet", pwd="Passw
               },
         )
     else:
+#        rep = post('transfer_coin',
+#            { "kribbz": kribbz,
+#              "transfer": transfer,
+#              "wallet":
+#                  {
+#                      "password":pwd,
+#                      "wallet_name": wal_name,
+#                      },
+#              },
+#        )
         rep = post('transfer_coin',
-            { "kribbz": kribbz,
-              "transfer": transfer,
+            { "kribbz": {
+                "transactionId":"20190110_1605", "streeAddress":"700 Rodeo Drive", "cityName":"Beverly Hills",
+                "stateCode":"CA","zipCode":"90210","latitude":"34.079678","longitude":"-118.413515",
+                "transactionDateTime":"1523855778",
+                "transactionTotal":"9975000.00",
+                "sellerName":"John Clark",
+                "buyerName":"Emily Stevens",
+                "agent_signature":"ckbzzBGZpWBZiPyQmxN3NCK8DJ8S37Vhxb69a9pbYXgMAwKKPDGeZ93aZgSXfX1E3GMEbk6tgLGPK8gDAeGquLmASXKvRim7pzN",
+                "agent_pkey":"ckbzzBGZpWBZiPyQmxN3NCK8DJ8S37Vhxb69a9pbYXgMAwKKPDGeZ93aZgSXfX1E3GMEbk6tgLGPK8gDAeGquLmASXKvRim7pzN",
+                "investor_signature":"ckbzzBGZpWBZiPyQmxN3NCK8DJ8S37Vhxb69a9pbYXgMAwKKPDGeZ93aZgSXfX1E3GMEbk6tgLGPK8gDAeGquLmASXKvRim7pzN",
+                "investor_pkey":"ckbzzBGZpWBZiPyQmxN3NCK8DJ8S37Vhxb69a9pbYXgMAwKKPDGeZ93aZgSXfX1E3GMEbk6tgLGPK8gDAeGquLmASXKvRim7pzN",
+                "owner_signature":"ckbzzBGZpWBZiPyQmxN3NCK8DJ8S37Vhxb69a9pbYXgMAwKKPDGeZ93aZgSXfX1E3GMEbk6tgLGPK8gDAeGquLmASXKvRim7pzN",
+                "owner_pkey":"ckbzzBGZpWBZiPyQmxN3NCK8DJ8S37Vhxb69a9pbYXgMAwKKPDGeZ93aZgSXfX1E3GMEbk6tgLGPK8gDAeGquLmASXKvRim7pzN",
+                },
+              "transfer": {
+                  "amount": 22.2222,
+                  "destination_address": "ckbzz6v3RFwV3s5je4PzcDGTZdsHy5Dg13dCrXwYDctYQi1UXYd3jVDPAfNHJmrSxx4TvMnh1pVUUNudRrqXEL7D46nY8D4Virp", #wallet24
+                  # "ckbzz6v3RFwV3s5je4PzcDGTZdsHy5Dg13dCrXwYDctYQi1UXYd3jVDPAfNHJmrSxx4TvMnh1pVUUNudRrqXEL7D46nY8D4Virp",  # wallet24
+                  #  "ckbzzAbqtqhcrN89awPysMBjQLZwAsVvGC8yFaa5hJ1E6cY6oHEsJyY6rjLCuXWRc1dWCXZSCecGMWUPYrpJPHYP9LkKo5Q8QNz", #wallet23
+                  # "ckbzz9ntUJj4mx3j75nAss3CzvCGXASnYTejDexmfC7k6f4XLqsMJUKN7i84SEz6CTR9LM4ggBMRUgXBHhAtUAgtCvWzv8dGjPr"  #wallet22
+                  # "ckbzzAkpo6cY3hRGrVhyYFNaH9uowjUDhVwN2APDo8TRjWsBewnM39i5MXxJmnDAJkbYFVA6rxMAuTSACbZbAMwT5FzK3sQnLro",  # wallet7
+                  # "ckbzz9ntUJj4mx3j75nAss3CzvCGXASnYTejDexmfC7k6f4XLqsMJUKN7i84SEz6CTR9LM4ggBMRUgXBHhAtUAgtCvWzv8dGjPr"
+                  # "ckbzz7dYVt6T4sBWhjp8wqQUMgoE8hWyfFCcBjjdLrzEdJU9SQsXjm73rb4VzZt7RxGzuLCAs4ZtQQMJzeELHP841TbYDt4Rm9V"  wal6
+                  "fee": 100000,
+                  "mixin": 0,
+                  },
               "wallet":
                   {
                       "password":pwd,
