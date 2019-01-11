233e7d972ef7a05db432223d2ee7bd00ea9c95c6
sanko61 <assem1@yandex.ru>
2019-01-11 16:00:26 +0300

    fix  search unittests with kribbz fields

--- test/search_kribbz.py
+++ test/search_kribbz.py
@@ -25,40 +25,48 @@ if __name__ == "__main__":
     print ("wallet24:==========================================================================")
     r1 = search_kribbz(w_name="wallet24", pwd="Password12345", stateCode="CA", zipCode="90210" )
     print (r1)
+    print ("cnt=", r1["cnt"])
 
     print ("kribbz2:==========================================================================")
     r1 = search_kribbz(w_name="kribbz2", pwd="Password12345", stateCode="CA", zipCode="90210" )
     print (r1)
+    print ("cnt=", r1["cnt"])
 
     time.sleep(2)
     print ("kribbz2:==========================================================================")
     r1 = search_kribbz(w_name="kribbz2", pwd="Password12345", stateCode="CA", zipCode="90210" )
     print (r1)
+    print ("cnt=", r1["cnt"])
 
     time.sleep(2)
     print ("==========================================================================")
     r2 = search_by_filter(w_name="wallet24", pwd="Password12345", filter={ "streeAddress":"700RodeoDrive"})
     print(r2)
+    print ("cnt=", r2["cnt"])
 
     time.sleep(2)
     print ("==========================================================================")
     r2 = search_by_filter(w_name="wallet24", pwd="Password12345", filter={ "streeAddress":"700RodeoDrive", "cityName":"BeverlyHills"})
     print(r2)
+    print ("cnt=", r2["cnt"])
 
     time.sleep(2)
     print ("==========================================================================")
     r2 = search_by_filter(w_name="wallet24", pwd="Password12345", filter={ "streeAddress":"700RodeoDrive", "cityName":"BeverlyHills", "latitude":"34.079678"})
     print(r2)
+    print ("cnt=", r2["cnt"])
 
     time.sleep(2)
     print ("==========================================================================")
     r2 = search_by_filter(w_name="wallet24", pwd="Password12345", filter={ "streeAddress":"700RodeoDrive", "cityName":"BeverlyHills", "latitude":"34.079678", "longitude":"-118.413515"})
     print(r2)
+    print ("cnt=", r2["cnt"])
 
     time.sleep(2)
     print ("==========================================================================")
     r2 = search_by_filter(w_name="wallet24", pwd="Password12345", filter={ "streeAddress":"700RodeoDrive", "cityName":"BeverlyHills", "latitude":"34.079678", "longitude":"-118.413515", "transactionDateTime":"1523855778"})
     print(r2)
+    print ("cnt=", r2["cnt"])
 
 #
 #"kribbz": {
